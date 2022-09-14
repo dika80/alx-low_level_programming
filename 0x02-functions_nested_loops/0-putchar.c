5d46b66d1e82c4bef9ee49aa655a1c7eb02c7471
dika80 <dikaikechukwu@gmail.com>
2022-09-13 21:16:46 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  * Return: 0 on success execution.
  */
 
-int main(void)
+int main(void);
 {
 	char s[] = "_putchar";
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0');
 	{
 		_putchar(s[i]);
 		i++;
