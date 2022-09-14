8320e18947b567a44386952698d4e19d5f0fac22
dika80 <dikaikechukwu@gmail.com>
2022-09-13 21:08:53 -0700

    Main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-int _putchar(cha;
+int _putchar(char;
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,25 +15,3 @@ int add(int, int);
 void print_to_98(int n);
 
 #endif
-#include "main.h"
-
-/**
- * main - prints the word _putchar
- *
- * Return: 0 on success execution.
- */
-
-int main(void)
-{
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
-	return (0);
-}
